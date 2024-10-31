8920c3d8f36efcb957a4dadbdbcabf37b5f766eb
David-Chazoule <d.chazoule@gmail.com>
2024-10-31 22:07:10 +0100

    recupération données crew

--- spacetourism/src/components/crew/Crew.tsx
+++ spacetourism/src/components/crew/Crew.tsx
@@ -1,9 +1,68 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-
-function Crew() {
-  return (
-    <div className="crew_container"></div>
-  )
+interface CrewMember {
+  name: string;
+  role: string;
+  bio: string;
+  images: {
+    png: string;
+    webp: string;
+  };
 }
 
-export default Crew
+function Crew() {
+  const [crew, setCrew] = useState<CrewMember[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("/data.json")
+      .then((response) => setCrew(response.data.crew))
+      .catch((error) =>
+        console.error("erreur lors du chargement du crew", error)
+      );
+  }, []);
+
+  const handleIndexChange = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  const currentMember = crew[currentIndex];
+
+  return (
+    <div className="crew_container">
+      <div className="title-box">
+        <h2>
+          <span>02</span> MEET YOUR CREW
+        </h2>
+      </div>
+
+      {currentMember && (
+        <div className="member_container">
+          <div className="member_information">
+            <h2>{currentMember.role}</h2>
+            <h1>{currentMember.name}</h1>
+            <p>{currentMember.bio}</p>
+            <div className="crew-controls">
+              {crew.map((_, index) => (
+                <button
+                  key={index}
+                  className={`crew-button ${
+                    index === currentIndex ? "active" : ""
+                  }`}
+                  onClick={() => handleIndexChange(index)}
+                ></button>
+              ))}
+            </div>
+          </div>
+          <div className="img-crew">
+            <img src={`${currentMember.images.png}`} alt="crew-img" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Crew;
--- spacetourism/src/components/home/Home.tsx
+++ spacetourism/src/components/home/Home.tsx
@@ -1,14 +1,21 @@
-
-
 function Home() {
   return (
-    <div className='home_container'>
-
-
-
-
+    <div className="home_container">
+      <div className="title_description-box">
+      <h5>So, you want to travel to</h5>
+      <h1>Space</h1>
+      <p>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
+      </p>
     </div>
-  )
+    <div>
+      <span>EXPLORE</span>
+    </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
