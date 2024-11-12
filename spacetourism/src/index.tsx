f09871cdef824e6b5bff07dd913f5e8ec632b718
David-Chazoule <d.chazoule@gmail.com>
2024-11-12 18:43:01 +0100

    modification browserRouter par hashRouter pour deployement

--- spacetourism/src/index.tsx
+++ spacetourism/src/index.tsx
@@ -4,17 +4,17 @@ import "./index.css";
 import "./styles/_index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
